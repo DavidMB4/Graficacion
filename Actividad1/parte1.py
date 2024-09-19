681c7e2f189fc232454b0d94df2485e2622e486f
David Maldonado <david.maldonado.4810@gmail.com>
2024-09-19 00:14:26 -0600

    Agregado en carpeta Actividad1

---
+++ Actividad1/parte1.py
@@ -0,0 +1,27 @@
+import cv2 as cv
+import numpy as np
+import math
+
+img=cv.imread('C:/Users/david/OneDrive/Documentos/Tec/5 Semestre/Graficacion/Dibujo pixel/pokeball.png', 0)
+x, y=img.shape
+center_X, center_y = (x // 2, y // 2)
+angle = 60
+scale_x, scale_y = 0.5, 0.5
+dx, dy = 10, 10
+
+img2 = np.zeros((x, y), dtype=np.uint8)
+xx, yy = img2.shape
+theta = math.radians(angle)
+
+for i in range(x):
+    for j in range(y):
+        
+        x2 = int((int((j - center_X) * math.cos(theta) + (i - center_y) * math.sin(theta) + center_X))*scale_x)+dx
+        y2 = int((int(-(j - center_X) * math.sin(theta) + (i - center_y) * math.cos(theta) + center_y))*scale_y)+dy
+        if 0 <= x2 < y and 0 <= y2 < x:
+            img2[x2, y2] = img[i, j]
+        
+cv.imshow('img', img)
+cv.imshow('img2', img2)
+cv.waitKey()
+cv.destroyAllWindows()
