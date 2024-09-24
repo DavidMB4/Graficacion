f2ac4dba647976bf355ef3d999268eb7e234f436
David Maldonado <david.maldonado.4810@gmail.com>
2024-09-23 22:18:33 -0600

    Sistema solar avance 2

--- ActividadSistemaSolar/sistemaSolar.py
+++ ActividadSistemaSolar/sistemaSolar.py
@@ -15,21 +15,49 @@ num_puntos = 1000
 venus_ejeMayor = 200
 venus_ejeMenor = 120
 
+tierra_ejeMayor = 220
+tierra_ejeMenor = 140
+
+marte_ejeMayor = 240
+marte_ejeMenor = 160
+
+jupiter_ejeMayor = 260
+jupiter_ejeMenor = 180
+
+saturno_ejeMayor = 280
+saturno_ejeMenor = 200
+
+urano_ejeMayor = 300
+urano_ejeMenor = 220
+
+neptuno_ejeMayor = 320
+neptuno_ejeMenor = 240
+
 t_vals = np.linspace(0, 2 * np.pi, num_puntos)
 
 for t in t_vals:
 
     imagen = np.zeros((img_height, img_width, 3), dtype=np.uint8)
     
-    t_mercurio = t * 1 
-    t_venus = t * 0.8  
+    t_mercurio = t * 1.6 
+    t_venus = t * 1.4  
+    t_tierra = t * 1.25  
+    t_marte = t * 1.18  
+    t_jupiter = t * 1 
+    t_saturno = t * 0.9  
+    t_urano = t * 0.7  
+    t_neptuno = t * 0.5 
 
     mercurioPunto = generar_punto_elipse(mercurio_ejeMayor, mercurio_ejeMenor, t_mercurio)
     veunsPunto = generar_punto_elipse(venus_ejeMayor, venus_ejeMenor, t_venus)
+    tierraPunto = generar_punto_elipse(tierra_ejeMayor, tierra_ejeMenor, t_tierra)
+    martePunto = generar_punto_elipse(marte_ejeMayor, marte_ejeMenor, t_marte)
     
 
     cv2.circle(imagen, mercurioPunto, radius=5, color=(126, 200, 243), thickness=-1)
     cv2.circle(imagen, veunsPunto, radius=10, color=(0, 100, 158), thickness=-1)
+    cv2.circle(imagen, tierraPunto, radius=10, color=(76, 201, 166), thickness=-1)
+    cv2.circle(imagen, martePunto, radius=10, color=(5, 80, 210), thickness=-1)
     
 
     for t_tray in t_vals:
@@ -38,6 +66,12 @@ for t in t_vals:
         
         pt_tray_Venus = generar_punto_elipse(venus_ejeMayor, venus_ejeMenor, t_tray)
         cv2.circle(imagen, pt_tray_Venus, radius=1, color=(255, 255, 255), thickness=-1)
+        
+        pt_tray_tierra = generar_punto_elipse(tierra_ejeMayor, tierra_ejeMenor, t_tray)
+        cv2.circle(imagen, pt_tray_tierra, radius=1, color=(255, 255, 255), thickness=-1)
+        
+        pt_tray_marte = generar_punto_elipse(marte_ejeMayor, marte_ejeMenor, t_tray)
+        cv2.circle(imagen, pt_tray_marte, radius=1, color=(255, 255, 255), thickness=-1)
     
 
     cv2.imshow('img', imagen)
