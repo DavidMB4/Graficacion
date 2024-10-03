38500eff6e1708372a7dc9061fa31ab3f00328ba
David Maldonado <david.maldonado.4810@gmail.com>
2024-10-02 23:49:19 -0600

    Actividad procesamiento de imagen correccion de grises

--- ActividadProcesamientoImg/procesamientoImagenVideo.py
+++ ActividadProcesamientoImg/procesamientoImagenVideo.py
@@ -10,7 +10,7 @@ while(True):
         #Imagen color Azul
         bajo_azul = np.array([85, 40, 40])
         alto_azul = np.array([129, 255, 255])
-        imagen_gris = cv2.cvtColor(hsv, cv2.COLOR_BGR2GRAY)
+        imagen_gris = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         imagen_gris_bgr = cv2.cvtColor(imagen_gris, cv2.COLOR_GRAY2BGR)
         
         
