3ca82bfe0728e588f2d611c7ea0aaad6d44ae1a3
David Maldonado <david.maldonado.4810@gmail.com>
2024-10-08 13:55:11 -0600

    Actividad contar el numero de objetos de un color avance en clase

---
+++ ActividadPreExamen/preExamen.py
@@ -0,0 +1,34 @@
+import cv2 as cv
+import numpy as np
+
+imagen = cv.imread('salida.png', 1)
+imagenHSV = cv.cvtColor(imagen, cv.COLOR_BGR2HSV)
+
+rangoRojoBajo = np.array(0, 40, 40)
+rangoRojoAlto = np.array(10, 255, 255)
+
+rangoRojoBajo2 = np.array(160, 40, 40)
+rangoRojoAlto2 = np.array(180, 255, 255)
+
+mascaraRojo1 = cv.inRange(imagenHSV, rangoRojoBajo, rangoRojoAlto)
+mascaraRojo2 = cv.inRange(imagenHSV, rangoRojoBajo2, rangoRojoAlto2)
+mascaraRojo = cv.add(mascaraRojo1, mascaraRojo2)
+
+rangoAzulBajo = np.array([85, 40, 40])
+rangoAzulAlto = np.array([128, 255, 255])
+
+mascaraAzul = cv.inRange(imagenHSV, rangoAzulBajo, rangoAzulAlto)
+
+rangoVerdeBajo = np.array([40, 40, 40])
+rangoVerdeAlto = np.array([80, 255, 255])
+
+mascaraVerde = cv.inRange(imagenHSV, rangoVerdeBajo, rangoVerdeAlto)
+
+rangoAmarilloBajo = np.array([22, 40, 40])
+rangoAmarilloAlto = np.array([32, 40, 40])
+
+mascaraAmarillo = cv.inRange(imagenHSV, rangoAmarilloBajo, rangoAmarilloAlto)
+
+cv.imshow('EJEMPLO', mascaraAzul)
+cv.waitKey(0)
+cv.destroyAllWindows()
